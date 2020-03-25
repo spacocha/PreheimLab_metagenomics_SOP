150780595c6116017e34c2ea88018d606995ccf7
Sarah Preheim <sprehei1@jhu.edu>
2020-03-25 17:20:33 -0400

    Added zipped trimmer and removed gzip from trimmer.sh

--- scripts/trimmer_zipped.sh
+++ scripts/trimmer_zipped.sh
@@ -16,9 +16,11 @@ FWD_FQ=`awk "NR==$SLURM_ARRAY_TASK_ID" $FORWARD`
 REV_FQ=`awk "NR==$SLURM_ARRAY_TASK_ID" $REVERSE`
 
 #This assumes they are zipped but you provide the unzipped name
+gunzip $LIB_DIR/${FWD_FQ}.gz
+gunzip $LIB_DIR/${REV_FQ}.gz
 
 trimmomatic PE -threads 8 -phred33 \
-${LIB_DIR}/${FWD_FQ}  ${LIB_DIR}/${REV_FQ} \
+$LIB_DIR/$FWD_FQ  $LIB_DIR/$REV_FQ \
 $ASSEM/${FWD_FQ}_s1_pe $ASSEM/${FWD_FQ}_s1_se $ASSEM/${REV_FQ}_s2_pe $ASSEM/${REV_FQ}_s2_se \
 ILLUMINACLIP:$AD_LOC:2:30:10 \
 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:33 MINLEN:50
